f36f32ddbe200c78f9e1d9a988638bb6554c04c4
Agustin Barrio <agustin.b@solunika.com>
2024-03-16 01:43:22 -0300

    fix placeholders

--- src/hooks/dynamicForms/useGetControllerInputProps.js
+++ src/hooks/dynamicForms/useGetControllerInputProps.js
@@ -8,25 +8,27 @@ function useGetControllerInputProps() {
     const getControllerInputProps = useCallback((input) => {
         let result = {}
         result.name = input.name
-        result.placeholder = input.placeholder
-        result.label = input.label ?? input.name
-
 
         //checkbox
         if (input?.type === INPUT_TYPES.CHECKBOX) {
             result.render = CheckboxInput
             result.defaultValue = input?.inputConfig?.defaultValue || false
+            result.label = input?.inputConfig?.label || input.name
         }
 
         //text
         if (input?.type === INPUT_TYPES.TEXT) {
             result.render = TextInput
             result.defaultValue = input?.inputConfig?.defaultValue || ''
+            result.label = input?.inputConfig?.label || input.name
+            result.placeholder = input?.inputConfig?.placeholder || ''
         }
 
         if (input?.type === INPUT_TYPES.NUMBER) {
             result.render = NumberInput
             result.defaultValue = input?.inputConfig?.defaultValue ?? 0
+            result.label = input?.inputConfig?.label || input.name
+            result.placeholder = input?.inputConfig?.placeholder || ''
         }
 
         return result
